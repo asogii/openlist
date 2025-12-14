681e3524f3faa4f72ffd673fb1844f6914d574e3
asogii <asogiix@gmail.com>
2025-12-14 23:42:33 +0800

    openlist

--- rebase_upstream.sh
+++ rebase_upstream.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git fetch alist --tags
+git fetch openlist --tags
 git checkout main
 #git rebase tags/v3.45.0
 git describe --tags `git rev-list --tags --max-count=1` > upstream_version
