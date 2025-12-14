c7accfe355178b460d8f2dcb6b73b816f9a44dfa
asogii <asogiix@gmail.com>
2025-12-14 23:46:40 +0800

    update upstream version

--- rebase_upstream.sh
+++ rebase_upstream.sh
@@ -4,5 +4,6 @@ git fetch openlist --tags
 git checkout main
 #git rebase tags/v3.45.0
 git describe --tags `git rev-list --tags --max-count=1` > upstream_version
+git commit -am "update upstream version"
 git rebase tags/$(cat upstream_version)
 
